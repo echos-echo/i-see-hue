91f12f5bf7795f1ef81483999036f52ab6d82543
Eve Cho <e.young.echo@gmail.com>
2022-09-19 04:14:29 -0400

    feature, Splash.js: more instructions added to the splash screen component for users

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -7,8 +7,11 @@ function Splash() {
     }
     return (
         <div id='splash'>
-            <h1>Hey this is some div</h1>
-            <button onClick={clickHandler}>Click to start game</button>
+            <h1>i see hue, can you?</h1>
+            <h2>The game is simple:</h2>
+            <h3>click and drag (or touch and drag if you have a touchscreen device) the colored tiles until they are in color order</h3>
+            <h3>the first and last colors do not move: use the two endpoints to drag your colors into the proper gradient</h3>
+            <button onClick={clickHandler}>unmix your hues</button>
         </div>
     )
 }
