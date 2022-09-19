48a134fdb733ae8fb0cf7b642dc24f00c44b6b35
Eve Cho <e.young.echo@gmail.com>
2022-09-19 00:59:20 -0400

    seemingly bug fixed: the colors array would randomly increase in size by itself after a few rounds of play; fixed by separating set functions into individual useEffect()s, but will keep an eye out

--- src/components/GameRow.js
+++ src/components/GameRow.js
@@ -1,5 +1,5 @@
 import {generateColors, checkSolution, sortColors} from '../createGame';
-import React from 'react';
+import React, { useEffect } from 'react';
 import SingleTile from './SingleTile';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -7,12 +7,12 @@ const _GAME_SIZE = 12;
 
 function GameRow() {
     // initial generated array of all colors
-    const [startingColors, setStartingColors] = React.useState(generateColors(10));
+    const [startingColors, setStartingColors] = React.useState(generateColors(_GAME_SIZE));
     // setting the solution array
     const [solution, setSolution] = React.useState(sortColors([...startingColors]));
     // the lowest and highest hue values; tiles not meant to be dragged
-    const [firstColor, setFirstColor] = React.useState(Math.min.apply(null, startingColors));
-    const [lastColor, setLastColor] = React.useState(Math.max.apply(null, startingColors));
+    const [firstColor, setFirstColor] = React.useState(Math.min(...startingColors));
+    const [lastColor, setLastColor] = React.useState(Math.max(...startingColors));
     const [colors, setColors] = React.useState(startingColors.filter(color => color !== firstColor && color !== lastColor));
     // states must be initialized in this order!!!
 
@@ -27,23 +27,33 @@ function GameRow() {
         newColors.splice(result.destination.index, 0, tileToRemove[0]);
         // finally, the updated array is set in our state
         setColors(newColors);
-        console.dir(newColors);
-        // console.dir(solution);
+        console.log(...newColors);
+        console.log(...solution);
         console.log(firstColor + ' and ' + lastColor)
     }
 
     function handleOnClick() {
-        setStartingColors(generateColors(10)); 
-        setSolution(sortColors([...startingColors]));
-        setFirstColor(Math.min.apply(null, startingColors));
-        setLastColor(Math.max.apply(null, startingColors));
-        setColors(startingColors.filter(color => color !== firstColor && color !== lastColor));
+        setStartingColors(generateColors(_GAME_SIZE)); 
+        // setSolution(sortColors([...startingColors]));
+        // setFirstColor(Math.min(...startingColors));
+        // setLastColor(Math.max(...startingColors));
+        // setColors(startingColors.filter(color => color !== firstColor && color !== lastColor));
     }
     
     React.useEffect(() => {
         if (checkSolution(colors, solution.slice(1, solution.length - 1))) window.alert('congrats');
     }, [colors, solution]);
-    
+
+    React.useEffect(() => {
+        setSolution(sortColors([...startingColors]));
+        setColors(startingColors.filter(color => color !== firstColor && color !== lastColor));
+    }, [startingColors, firstColor, lastColor]);
+
+    React.useEffect(() => {
+        setFirstColor(Math.min(...startingColors));
+        setLastColor(Math.max(...startingColors));
+    }, [startingColors])
+
     return (
         <DragDropContext id='play' onDragEnd={handleOnDragEnd}>
             <Droppable droppableId='game-row' direction='horizontal'>
@@ -53,7 +63,7 @@ function GameRow() {
                         {colors.filter(color => color !== firstColor && color !== lastColor).map((color, index) => 
                         <Draggable key={color.toString()} draggableId={color.toString()} isDragDisabled={checkSolution(colors, solution.slice(1, solution.length - 1))} index={index}>
                             {provided => (
-                                <SingleTile innerRef={provided.innerRef} provided={provided} color={color}/>
+                                <SingleTile id={color} innerRef={provided.innerRef} provided={provided} color={color}/>
                             )}
                         </Draggable>)}
                         {provided.placeholder}
