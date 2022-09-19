08063ab1b6a0200d7b91a29ec5f07ef1ef73a0bd
Eve Cho <e.young.echo@gmail.com>
2022-09-18 20:21:40 -0400

    more added to state in order to handle loading a next round of tile colors: solution updates, colors updates, and the initial generated colors update

--- src/components/GameRow.js
+++ src/components/GameRow.js
@@ -7,29 +7,30 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function GameRow() {
     const dispatch = useDispatch();
-    const [colors, setColors] = React.useState(useSelector(state => state.tiles) || generateColors(10));
-    const solution = sortColors([...colors]);
+    const [startingColors, setStartingColors] = React.useState(generateColors(10));
+    const [solution, setSolution] = React.useState(sortColors([...startingColors]));
     // the lowest and highest hue values; tiles not meant to be dragged
-    const firstColor = Math.min.apply(null, colors);
-    const lastColor = Math.max.apply(null, colors);
+    const firstColor = Math.min.apply(null, startingColors);
+    const lastColor = Math.max.apply(null, startingColors);
+    const [colors, setColors] = React.useState(startingColors.filter(color => color !== firstColor && color !== lastColor));
 
     function handleOnDragEnd(result) {
         // creates a new array using our base state colors
         const newColors = [...colors];
         // the tile we are dragging out of a spot
-        const tileToRemove = newColors.splice(result.source.index + 1, 1);
-        console.log(tileToRemove + ' removed')
+        const tileToRemove = newColors.splice(result.source.index, 1);
         // inserts the dragged tile into the destination index
         // refer to the result object for more information on how to utilize handleOnDragEnd
-        newColors.splice(result.destination.index + 1, 0, tileToRemove[0]);
+        newColors.splice(result.destination.index, 0, tileToRemove[0]);
         // finally, the updated array is set in our state
         setColors(newColors);
-        return checkSolution(colors, solution) ? window.alert('congrats') : null;
+        console.dir(newColors);
+        console.dir(solution.slice(1, solution.length - 1))
+        return checkSolution(colors, solution.slice(1, solution.length - 1)) ? window.alert('congrats') : null;
     }
     
     React.useEffect(() => {
-        dispatch(updateSolution(colors));
-        dispatch(updateCurrentTiles(colors));
+        return checkSolution(colors, solution.slice(1, solution.length - 1)) ? window.alert('congrats') : null;
     }, [colors, dispatch]);
     
     return (
@@ -39,7 +40,7 @@ function GameRow() {
                     <div {...provided.droppableProps} ref = {provided.innerRef} id='game-row'>
                         <SingleTile id={firstColor} color={firstColor} style={{backgroundColor: `hsl(${firstColor}, 50%, 50%)`}}/>
                         {colors.filter(color => color !== firstColor && color !== lastColor).map((color, index) => 
-                        <Draggable key={color.toString()} draggableId={color.toString()} isDragDisabled={checkSolution(colors, solution)} index={index+1}>
+                        <Draggable key={color.toString()} draggableId={color.toString()} isDragDisabled={checkSolution(colors, solution.slice(1, solution.length - 1))} index={index}>
                             {provided => (
                                 <SingleTile id={color} style={{backgroundColor: `hsl(${color}, 50%, 50%)`}} innerRef={provided.innerRef} provided={provided} color={color}/>
                             )}
@@ -50,7 +51,10 @@ function GameRow() {
                 )}
             </Droppable>
             <hr/>
-            <button id="next-round" onClick={() => setColors(generateColors(10))}>Next Round of Colors</button>
+            <button id="next-round" onClick={() => {
+                setStartingColors(generateColors(10)); 
+                setColors(startingColors.filter(color => color !== firstColor && color !== lastColor));
+                setSolution(sortColors([...startingColors]))}}>Next Round of Colors</button>
         </DragDropContext>
   );
 }
