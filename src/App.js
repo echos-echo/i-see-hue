5fcf30f90d1106cd51a6f289ee14ae45c37b3640
Eve Cho <e.young.echo@gmail.com>
2022-11-07 00:55:37 -0500

    feature: state for game difficulty and select dropdown added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import GameRow from './components/GameRow';
 import Splash from './components/Splash'
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('medium');
+
   return (
     <div className="content">
       <Splash/>
       <div id="header">
         <h1>Test Your Color Perception</h1>
         <h2>Click and Drag the tiles so the colors appear in gradient order</h2>
+        <select>
+          <option value='easy'>Easy</option>
+          <option value='medium'>Medium</option>
+          <option value='hard'>Hard</option>
+          <option value='extra'>Hard+++</option>
+        </select>
         <GameRow/>
       </div>
     </div>
